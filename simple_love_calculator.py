0b0884754c26141dce5c85c92225f9bfbb6022e8
Mh Limon <mhlimon571@gmail.com>
2022-11-19 01:05:17 +0600

    second commit

---
+++ simple_love_calculator.py
@@ -0,0 +1,9 @@
+import random
+
+random_fl = random.random()
+random_float = random_fl * 5
+print(random_float)
+random_int = random.randint(1,100)
+print(random_int)
+love_score  = random.randint(1,100)
+print(f"Love {love_score}")
